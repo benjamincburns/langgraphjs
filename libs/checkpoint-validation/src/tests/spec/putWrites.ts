74160997af97fbbc7260c630a24e007fa1b2ce14
Ben Burns <803016+benjamincburns@users.noreply.github.com>
2024-10-08 13:36:03 +1300

    squashme: clean-up putWrites tests

--- libs/checkpoint-validation/src/tests/spec/putWrites.ts
+++ libs/checkpoint-validation/src/tests/spec/putWrites.ts
@@ -1,16 +1,11 @@
 import {
   Checkpoint,
   CheckpointMetadata,
+  CheckpointTuple,
   uuid6,
   type BaseCheckpointSaver,
 } from "@langchain/langgraph-checkpoint";
-import {
-  describe,
-  it,
-  beforeEach,
-  afterEach,
-  expect,
-} from "@jest/globals";
+import { describe, it, beforeEach, afterEach, expect } from "@jest/globals";
 import { mergeConfigs, RunnableConfig } from "@langchain/core/runnables";
 import { CheckpointSaverTestInitializer } from "../../types.js";
 import { emptyInitialCheckpointTuple } from "./data.js";
@@ -21,84 +16,134 @@ export function putWritesTests<T extends BaseCheckpointSaver>(
 ) {
   describe(`${name}#putWrites`, () => {
     let saver!: T;
-    let config!: RunnableConfig;
-    let thread_id!: string;
-    let checkpoint_id!: string;
-    let metadata!: CheckpointMetadata;
-    let checkpoint!: Checkpoint;
-
-    beforeEach(async () => {
-      thread_id = uuid6(-3);
-      checkpoint_id = uuid6(-3);
+    let initializerConfig!: RunnableConfig;
+    const thread_id = uuid6(-3);
+    const checkpoint_id = uuid6(-3);
 
       const baseConfig = {
         configurable: {
           thread_id,
+
+          // this is here to make sure that the saver stores the whole config object, and not just the keys it knows about
+          some_random_key: "some_random_value",
         },
       };
 
-      const initializerConfig = await initializer.configure?.(config);
 
-      config = mergeConfigs(baseConfig, initializerConfig);
-      saver = await initializer.createSaver(config);
-      
-      const checkpointTuple = emptyInitialCheckpointTuple(checkpoint_id, "root", config);
-      checkpoint = checkpointTuple.checkpoint;
-      metadata = checkpointTuple.metadata!;
+    beforeEach(async () => {
+      initializerConfig = mergeConfigs(
+        baseConfig,
+        await initializer.configure?.(baseConfig)
+      );
+      saver = await initializer.createSaver(initializerConfig);
     });
 
     afterEach(async () => {
-      await initializer.destroySaver?.(saver, config);
+      await initializer.destroySaver?.(saver, initializerConfig);
     });
 
+
     describe.each(["root", "child"])("namespace: %s", (namespace) => {
-      const checkpoint_ns = namespace === "root" ? '' : namespace;
-    
-      beforeEach(async () => {
-        config = mergeConfigs(config, {
-          configurable: { checkpoint_ns },
+      const checkpoint_ns = namespace === "root" ? "" : namespace;
+      let configArgument: RunnableConfig;
+      let checkpoint: Checkpoint;
+      let metadata: CheckpointMetadata | undefined;
+
+
+      describe("success cases", () => {
+        let returnedConfig!: RunnableConfig;
+        let savedCheckpointTuple: CheckpointTuple | undefined;
+
+        beforeEach(async () => {
+          ({ checkpoint, metadata } = emptyInitialCheckpointTuple(
+            checkpoint_id,
+            checkpoint_ns,
+            initializerConfig
+          ));
+
+          configArgument = mergeConfigs(initializerConfig, {
+            configurable: { checkpoint_ns },
+          });
+
+          // ensure the test checkpoint does not already exist
+          const existingCheckpoint = await saver.get(
+            mergeConfigs(configArgument, {
+              configurable: {
+                checkpoint_id,
+              },
+            })
+          );
+          expect(existingCheckpoint).toBeUndefined(); // our test checkpoint should not exist yet
+
+          returnedConfig = await saver.put(
+            configArgument,
+            checkpoint,
+            metadata!,
+            {} /* not sure what to do about newVersions, as it's unused */
+          );
+
+          await saver.putWrites(
+            mergeConfigs(configArgument, returnedConfig),
+            [["animals", "dog"]],
+            "pet_task"
+          );
+
+          savedCheckpointTuple = await saver.getTuple(
+            mergeConfigs(configArgument, returnedConfig)
+          );
+
+          // fail here if `put` or `getTuple` is broken so we don't get a bunch of noise from the actual test cases below
+          expect(savedCheckpointTuple).not.toBeUndefined();
+          expect(savedCheckpointTuple?.checkpoint).toEqual(checkpoint);
+          expect(savedCheckpointTuple?.metadata).toEqual(metadata);
+          expect(savedCheckpointTuple?.config).toEqual(
+            expect.objectContaining( // allow the saver to add additional fields to the config
+              mergeConfigs(configArgument, { configurable: { checkpoint_id } })
+            )
+          );
         });
 
-        // ensure the test checkpoint does not already exist
-        const existingCheckpoint = await saver.get(config);
-        expect(existingCheckpoint).toBeUndefined(); // our test checkpoint should not exist yet
-        
-        config = mergeConfigs(config, await saver.put(config, checkpoint, metadata, {}));
-      });
-      
-      it("should store writes to the checkpoint", async () => {
-        await saver.putWrites(config, [["animals", "dog"]], "pet_task");
-        const checkpointTuple = await saver.getTuple(config);
-        expect(checkpointTuple).not.toBeUndefined();
-        expect(checkpointTuple?.checkpoint).toEqual(checkpoint);
-        expect(checkpointTuple?.metadata).toEqual(metadata);
-        expect(checkpointTuple?.config).toEqual(config);
-        expect(checkpointTuple?.pendingWrites).toEqual([["pet_task", "animals", "dog"]]);
-        expect(checkpointTuple?.parentConfig).toBeUndefined();
+        it("should store writes to the checkpoint", async () => {
+          expect(savedCheckpointTuple?.pendingWrites).toEqual([
+            ["pet_task", "animals", "dog"],
+          ]);
+        });
       });
 
-      it("should fail if the thread_id is missing", async () => {
-        const missingThreadIdConfig: RunnableConfig = mergeConfigs(config, {});
-        delete missingThreadIdConfig.configurable?.thread_id;
+      describe("failure cases", () => {
+        it("should fail if the thread_id is missing", async () => {
+          const missingThreadIdConfig: RunnableConfig = mergeConfigs(
+            configArgument,
+            {}
+          );
+          delete missingThreadIdConfig.configurable?.thread_id;
 
-        await expect(
-          async () =>
-            await saver.putWrites(
-              missingThreadIdConfig, [["animals", "dog"]], "pet_task"
-            )
-        ).rejects.toThrow();
-      });
+          await expect(
+            async () =>
+              await saver.putWrites(
+                missingThreadIdConfig,
+                [["animals", "dog"]],
+                "pet_task"
+              )
+          ).rejects.toThrow();
+        });
 
-      it("should fail if the checkpoint_id is missing", async () => {
-        const missingCheckpointIdConfig: RunnableConfig = mergeConfigs(config, {});
-        delete missingCheckpointIdConfig.configurable?.checkpoint_id;
+        it("should fail if the checkpoint_id is missing", async () => {
+          const missingCheckpointIdConfig: RunnableConfig = mergeConfigs(
+            configArgument,
+            {}
+          );
+          delete missingCheckpointIdConfig.configurable?.checkpoint_id;
 
-        await expect(
-          async () =>
-            await saver.putWrites(
-              missingCheckpointIdConfig, [["animals", "dog"]], "pet_task"
-            )
-        ).rejects.toThrow();
+          await expect(
+            async () =>
+              await saver.putWrites(
+                missingCheckpointIdConfig,
+                [["animals", "dog"]],
+                "pet_task"
+              )
+          ).rejects.toThrow();
+        });
       });
     });
   });
